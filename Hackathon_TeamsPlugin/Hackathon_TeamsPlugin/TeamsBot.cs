5b60705a5531acb2e0365feae298dbab8df5b834
Nishant Gupta <guptanishant@microsoft.com>
2022-09-19 10:45:32 +0530

    First checking of framework of the app

---
+++ Hackathon_TeamsPlugin/Hackathon_TeamsPlugin/TeamsBot.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Schema;
+
+namespace Hackathon_TeamsPlugin
+{
+    /// <summary>
+    /// An empty bot handler.
+    /// You can add your customization code here to extend your bot logic if needed.
+    /// </summary>
+    public class TeamsBot : TeamsActivityHandler
+    {
+        protected override Task OnEventActivityAsync(ITurnContext<IEventActivity> turnContext, CancellationToken cancellationToken)
+        {
+            return base.OnEventActivityAsync(turnContext, cancellationToken);
+        }
+
+        protected override Task<InvokeResponse> OnInvokeActivityAsync(ITurnContext<IInvokeActivity> turnContext, CancellationToken cancellationToken)
+        {
+            return base.OnInvokeActivityAsync(turnContext, cancellationToken);
+        }
+    }
+}
